136f9628c5196cea3938c07795c934bab1da577a
Scott Warwick <>
2019-03-07 11:58:40 +0100

    Update Criminals script

--- Criminals/README.md
+++ Criminals/README.md
@@ -1,11 +1,11 @@
 ## Criminals
-### Version: 1.0
+### Version: 1.1
 
 ![Example of the script in action](https://i.imgur.com/9wvbkro.png)
 
 ![Another example of the script in action](https://i.imgur.com/RKjTDpf.png)
 
-The script enhances the bounty system by adding global messages when people reach certain tresholds of bounty (500, 1000 and 5000) or when they manage to clear their name. It also adds the ability to award the bounty to players who kill the criminals. Finally, it is possible to append prefixes to the chat messages sent by criminals, to let others know they indeed broke the law.
+The script enhances the bounty system by adding global messages when people reach certain tresholds of bounty (defined at the top of the script) or when they manage to clear their name. It also adds the ability to award the bounty to players who kill the criminals. Finally, it is possible to append prefixes to the chat messages sent by criminals, to let others know they indeed broke the law.
 
 
 ## INSTALLING INSTRUCTIONS
@@ -16,40 +16,48 @@ The script enhances the bounty system by adding global messages when people reac
 
 3) Save `server.lua`, open `eventHandler.lua`.
 
-4) Find `Players[pid]:Message("You have successfully logged in.\n")` and below it add `criminals.OnLogin(pid)`.
+4) Find `Players[pid]:SaveBounty()` and add `criminals.UpdateBounty(pid)` below it.
 
-5) Find `Players[pid]:SaveBounty()` and add `criminals.UpdateBounty(pid)` below it.
-
-6) (OPTIONAL) Find `message = config.rankColors.moderator .. "[Mod] " .. message`. Below the `end` line add:
+5) (OPTIONAL) Find `message = config.rankColors.moderator .. "[Mod] " .. message`. Below the `end` line add:
 ```
-            local prefix = criminals.IsCriminal(pid)
+            local prefix = criminals.GetPrefix(pid)
             message = prefix .. message
 ```
 to add prefixes to in-game chat messages.
 
-7) Save `eventHandler.lua` and open `\player\base.lua`.
+6) Save `eventHandler.lua` and open `\player\base.lua`.
 
-8) Find `local killerPid = tes3mp.GetPlayerKillerPid(self.pid)` and below it add `criminals.ProcessBountyReward(self.pid, logicHandler.GetChatName(killerPid))`.
+7) Find `self:LoadCell()` and above it add `criminals.OnConnect(self.pid)`.
 
-9) Save `base.lua` and start the server. Try committing a crime to gain a bounty of 500 or more and see if a message is displayed in the chat.
+8) Find `if config.defaultSpawnCell ~= nil then` and above it add `criminals.OnConnect(self.pid)`.
+
+9) Find `local killerPid = tes3mp.GetPlayerKillerPid(self.pid)` and below it add `criminals.ProcessBountyReward(self.pid, killerPid)`.
+
+10) Save `base.lua` and start the server. Try committing a crime serious enough to be detected by the script to ensure that everything is working as intended.
 
 ## SETTINGS
-displayGlobalWanted = true -- whether or not to display the global messages in chat when a player's wanted status changes
-displayGlobalClearedBounty = true -- whether or not to display a global message when a player clears their bounty
-displayGlobalBountyClaim = true -- whether or not to display a global message when another player claims a bounty
-bountyItem = "gold_001" -- item used as bounty, in case you use a custom currency
+
 ### Changes you can make in the script
 |Variable|Description|
 |:----|:-----|
+|bountyThresholds|Bounty threshold values for each criminal level.|
+|bountyTitles|Titles for each criminal level, used in prefix.|
+|wantedMessages|Messages to display along with `[Notice] <playername>` every time a player reaches a new criminal level.|
 |displayGlobalWanted|Whether or not to display a message when a player becomes wanted.|
 |displayGlobalClearedBounty|Whether or not to display a message when a player clears their name.|
 |displayGlobalBountyClaim|Whether or not to display a message when a player claims a bounty by killing another player.|
 |bountyItem|Item used as bounty (normally gold).|
 
+Other messages displayed in chat can be found in various functions in the script. 
 ## KNOWN ISSUES
 
 The chat prefixes, although completely optional, can interfere with other scripts that add prefixes. Consult `#scripting_help` channel on TES3MP discord server if you happen to use another script which adds prefixes for a solution.
 
 ## CHANGELOG:
+### 1.1:
+[Fix crashes due to undefined custom variable.](https://github.com/quickstraw/tes3mp-scripts)
+
+Allow custom bounty thresholds and titles without rewriting entire script.
+
 ### 1.0:
 Script updated for 0.7.x.
--- Criminals/criminals.lua
+++ Criminals/criminals.lua
@@ -1,92 +1,77 @@
 ------------------
--- Version: 1.0 --
+-- Version: 1.1 --
 ------------------
 
 -- Settings
-displayGlobalWanted = true -- whether or not to display the global messages in chat when a player's wanted status changes
-displayGlobalClearedBounty = true -- whether or not to display a global message when a player clears their bounty
-displayGlobalBountyClaim = true -- whether or not to display a global message when another player claims a bounty
-bountyItem = "gold_001" -- item used as bounty, in case you use a custom currency
+local bountyThresholds = {500, 1000, 5000} -- thresholds for different criminal levels
+local bountyTitles = {"Thief","Criminal","Fugitive"} -- names for different levels
+local wantedMessages = {"has been declared a thief.","is now a wanted criminal.","has a death warrant. This criminal scum shall be killed on sight."} -- custom messages to display when player reaches each criminal level (with [notice] <playername> prefix)
+local displayGlobalWanted = true -- whether or not to display the global messages in chat when a player's wanted status changes
+local displayGlobalClearedBounty = true -- whether or not to display a global message when a player clears their bounty
+local displayGlobalBountyClaim = true -- whether or not to display a global message when another player claims a bounty
+local bountyItem = "gold_001" -- item used as bounty, in case you use a custom currency
 require("color")
 
 local Methods = {}
-Methods.OnLogin = function(pid) -- set the criminal level as a custom variable for players based on their bounty
-    if Players[pid].data.customVariables.Skvysh == nil then
-        Players[pid].data.customVariables.Skvysh = {}
-    end
-    if Players[pid].data.customVariables.Skvysh.criminal == nil then
-        local criminal
-        local bounty = Players[pid].data.fame.bounty
-        if bounty >= 5000 then
-            criminal = 3
-        elseif bounty >= 1000 then
-            criminal = 2
-        elseif bounty >= 500 then
-            criminal = 1
-        else
-            criminal = 0
-        end
-        Players[pid].data.customVariables.Skvysh.criminal = criminal
-    end
+-- update variables on login/register
+Methods.OnConnect = function(pid)
+    criminals.DefineData(pid)
+    criminals.GetNewCriminalLevel(pid)
 end
 
-Methods.IsCriminal = function(pid) -- get criminal prefix for chat messages
-    local bounty = tes3mp.GetBounty(pid)
+-- return formatted prefix
+Methods.GetPrefix = function(pid)
+    local criminalLevel = criminals.GetCriminalLevel(pid)
     local prefix = ""
-    if bounty >= 5000 then
-        prefix = color.Red .. "[Fugitive] " .. color.Default
-    elseif bounty >= 1000 then
-        prefix = color.Salmon .. "[Criminal] " .. color.Default
-    elseif bounty >= 500 then
-        prefix = color.LightSalmon .. "[Thief] " .. color.Default
+    if criminalLevel ~= 0 then
+        prefix = color.Salmon .. "["
+        if bountyTitles[criminalLevel] ~= nil then
+            prefix = prefix .. bountyTitles[criminalLevel]
+        else
+          prefix = prefix .. "N'wah"
+        end
+        prefix = prefix .. "]" .. color.Default
     end
     return prefix
 end
 
-Methods.GetNewCriminalLevel = function(pid) -- get the criminal level based on current bounty and previous level
+-- calculate current criminal level
+Methods.GetCriminalLevel = function(pid)
     local bounty = tes3mp.GetBounty(pid)
-    local previousCriminal = Players[pid].data.customVariables.Skvysh.criminal
-    local criminal
-    if bounty >= 5000 then
-        if previousCriminal ~= 3 then
-            criminal = 3
-        end
-    elseif bounty >= 1000 then
-        if previousCriminal ~= 2 then
-            criminal = 2
-        end
-    elseif bounty >= 500 then
-        if previousCriminal ~= 1 then
-            criminal = 1
-        end
-    elseif bounty == 0 then
-        if previousCriminal ~= 0 then
-            criminal = 0
+    local criminalLevel = 0
+    local i
+    for i = 1, #bountyThresholds do
+        if bounty >= bountyThresholds[i] then
+            criminalLevel = criminalLevel + 1
         end
     end
-    if criminal == nil then
+    return criminalLevel
+end
+
+-- check if criminal level is updated
+Methods.GetNewCriminalLevel = function(pid)
+    local criminal = criminals.GetCriminalLevel(pid)
+    local previousCriminal = Players[pid].data.customVariables.Skvysh.criminal
+    if criminal == previousCriminal then -- if current level is the same as previous one, we dont want to do any updates
         criminal = -1
     else
-        Players[pid].data.customVariables.Skvysh.criminal = criminal
+        Players[pid].data.customVariables.Skvysh.criminal = criminal -- store new criminal level as previous one
     end
     return criminal
 end
 
-Methods.UpdateBounty = function(pid) -- display global messages if needed when a criminal level changes
+-- display messages regarding criminal level changes
+Methods.UpdateBounty = function(pid)
     local message
     local playerName = tes3mp.GetName(pid)
     local criminal = criminals.GetNewCriminalLevel(pid)
     if criminal > 0 then
         if displayGlobalWanted == true then
             message = color.Crimson .. "[Alert] " .. color.Brown .. playerName .. " " .. color.Default
-            if criminal == 1 then
-                message = message .. "has been declared a thief.\n"
-            elseif criminal == 2 then
-                message = message .. "is now a wanted criminal.\n"
-            elseif criminal == 3 then
-                message = message .. "has a death warrant. This criminal scum shall be killed on sight.\n"
+            if wantedMessages[criminal] ~= nil then
+                message = message .. wantedMessages[criminal] .. "\n"
             else
-                message = ""
+                message = message .. "is an n'wah at large.\n"
             end
             tes3mp.SendMessage(pid, message, true)
         end
@@ -99,48 +84,39 @@ Methods.UpdateBounty = function(pid) -- display global messages if needed when a
     end
 end
 
-Methods.ProcessBountyReward = function(pid, killer) -- give rewards for claiming a bounty
+-- award the bounty to the killer, lower bounty of the killed player, display messages
+Methods.ProcessBountyReward = function(pid, killerPid)
     local playerName = tes3mp.GetName(pid)
-    local lastPid = tes3mp.GetLastPlayerId()
-    local killerPid = -1
+    local killerName = tes3mp.GetName(killerPid)
     local currentBounty = tes3mp.GetBounty(pid)
     local newBounty
     local reward
     local message
-    if currentBounty >= 500 then -- don't want newbies losing gold over petty theft
-        for i = 0, lastPid do
-            if Players[i] ~= nil and Players[i]:IsLoggedIn() then
-                if tostring(Players[i].name) == tostring(killer) then
-                    killerPid = Players[i].pid -- get killer's PID, assuming it was an actual player
-                    break
-                end
+    if pid ~= killerPid then -- don't want players clearing their own bounty by suicide
+        if currentBounty > 499 then -- don't want newbies losing gold over petty theft
+        if bountyItem ~= "" then
+            if tableHelper.containsKeyValue(Players[pid].data.inventory, "refId", bountyItem, true) then
+                itemIndex = tableHelper.getIndexByNestedKeyValue(Players[pid].data.inventory, "refId", bountyItem)
+                itemCount = Players[pid].data.inventory[itemIndex].count -- find how much gold the player has
+            else
+                itemCount = 0
             end
-        end
-        if killerPid ~= -1 then -- if a killer was found
-            if bountyItem ~= "" then
-                if tableHelper.containsKeyValue(Players[pid].data.inventory, "refId", bountyItem, true) then
-                    itemIndex = tableHelper.getIndexByNestedKeyValue(Players[pid].data.inventory, "refId", bountyItem)
-                    itemCount = Players[pid].data.inventory[itemIndex].count -- find how much gold the player has
-                else
-                    itemCount = 0
-                end
-                if itemCount >= currentBounty then -- if a bounty can be fully cleared, do so
+            if itemCount >= currentBounty then -- if a bounty can be fully cleared, do so
                     newBounty = 0
                     reward = currentBounty
                 else
                     newBounty = currentBounty - itemCount -- otherwise, clear it only partially
                     reward = itemCount
                 end
-                local structuredItem = { refId = bountyItem, count = reward, charge = -1 } -- give the reward to the killer
-                table.insert(Players[killerPid].data.inventory, structuredItem)
-                if itemCount ~= 0 then -- if the player actually has gold
+            local structuredItem = { refId = bountyItem, count = reward, charge = -1 } -- give the reward to the killer
+            table.insert(Players[killerPid].data.inventory, structuredItem)
+            if itemCount ~= 0 then -- if the player actually has gold
                     Players[pid].data.inventory[itemIndex].count = Players[pid].data.inventory[itemIndex].count - reward --remove the gold
                     if Players[pid].data.inventory[itemIndex].count == 0 then
                         Players[pid].data.inventory[itemIndex] = nil
                     end
                     if displayGlobalBountyClaim == true then -- display messages
-                        local message2 = killer .. " " .. tostring(reward)
-                        message = color.Brown .. killer .. color.Default .. " has claimed a bounty of " .. tostring(reward) .. " by killing " .. color.Brown .. Players[pid].name .. color.Default .. ".\n"
+                        message = color.Green .. "[Notice] " .. color.Brown .. killerName .. color.Default .. " has claimed a bounty of " .. tostring(reward) .. " by killing " .. color.Brown .. Players[pid].name .. color.Default .. ".\n"
                         tes3mp.SendMessage(pid, message, true)
                     else
                         message = color.Brown .. "You" .. color.Default .. " have claimed a bounty of " .. tostring(reward) .. " by killing " .. color.Brown .. Players[pid].name .. color.Default .. ".\n"
@@ -154,7 +130,7 @@ Methods.ProcessBountyReward = function(pid, killer) -- give rewards for claiming
                         end
                     end
                     Players[pid].data.fame.bounty = newBounty -- set new bounty
-                    tes3mp.SetBounty(pid, Players[pid].data.fame.bounty)
+                    tes3mp.SetBounty(pid, newBounty)
                     tes3mp.SendBounty(pid)
                     Players[pid]:LoadInventory() -- save inventories for both players
                     Players[pid]:LoadEquipment()
@@ -164,9 +140,21 @@ Methods.ProcessBountyReward = function(pid, killer) -- give rewards for claiming
                     Players[killerPid]:Save()
                     criminals.GetNewCriminalLevel(pid)
                 end
-            end
         end
     end
+    end
+end
+
+-- define custom variable if there is none, return it if it exists
+Methods.DefineData = function(pid)
+    local data = Players[pid].data.customVariables
+    if data.Skvysh == nil then
+        data.Skvysh = {}
+    end
+    if data.Skvysh.criminal == nil then
+        data.Skvysh.criminal = 0
+    end
+    return data.Skvysh.criminal
 end
 
 return Methods
